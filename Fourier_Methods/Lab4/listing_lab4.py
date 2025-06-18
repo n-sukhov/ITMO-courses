adf30ac9c84f7214218498470a9722e37fe98151
nsukhov <nsukhov22@gmail.com>
2025-06-18 22:24:12 +0300

    FM: lab4

--- Fourier_Methods/Lab4/listing_lab4.py
+++ Fourier_Methods/Lab4/listing_lab4.py
@@ -209,8 +209,133 @@ draw_plots(
     legend_loc='upper right'
 )
 
-# %%
+# %% [markdown]
+# # Задание 1.2
 
+# %%
+a1 = 0
+a2 = 5
+b2 = 4
+a = 4
+b = 0
+c_range = [0, 0.5, 2]
+d_range = [5, 10, 20]
+b1_range = [5, 14.14, 20]
+
+N = 2 ** 15
+t = np.linspace(0, 20, N)
+dt = t[1] - t[0]
+
+time_subplot_data = []
+spectrum_subplot_data = []
+freq_response_subplot_data = []
+
+for i, b1 in enumerate(b1_range):
+    time_row = []
+    spectrum_row = []
+    freq_response_row = []
+    
+    for j, c in enumerate(c_range):
+        for k, d in enumerate(d_range):
+            g_signal = np.array([g(ti, a) for ti in t])
+            u_signal = np.array([u(ti, a, b, c, d) for ti in t])
+            
+            sys = signal.TransferFunction([1, a1, a2], [1, b1, b2])
+            _, y_signal, _ = signal.lsim(sys, U=u_signal, T=t)
+            
+            freq_g, func_g = calc_spectrum(g_signal, dt)
+            freq_u, func_u = calc_spectrum(u_signal, dt)
+            freq_y, func_y = calc_spectrum(y_signal, dt)
+            
+            w = freq_g
+            W_mag = np.abs((1j*w)**2 + a1*(1j*w) + a2) / np.abs((1j*w)**2 + b1*(1j*w) + b2)#???
+            
+            afc_mask = (freq_g >= 0) & (freq_g < 30)
+            omega_mask = np.abs(freq_g) < 30
+            t_mask = t < 7
+
+            time_plot = [
+                [t[t_mask]] * 3,
+                [u_signal[t_mask], g_signal[t_mask], y_signal[t_mask]],
+                ['u(t)', 'g(t)', 'y(t), filtered'],
+                't', 'Amplitude',
+                ['g', 'b', 'r'],
+                ['-'] * 3,
+                [1.0, 1.0, 1.0],
+                [None] * 3,
+                [None] * 3,
+                f'b1={b1:.2f}, c={c}, d={d}',
+            ]
+            time_row.append(time_plot)
+            
+            spectrum_plot = [
+                [freq_g[omega_mask], freq_u[omega_mask], freq_y[omega_mask]],
+                [2.0 / N * func_g[omega_mask],
+                 2.0 / N * func_u[omega_mask],
+                 2.0 / N * func_y[omega_mask]],
+                ['|g_hat(ω)|', '|u_hat(ω)|', 'y_hat(ω)|'],
+                'ω', 'Amplitude',
+                ['b', "#07BE01", 'r'],
+                ['-', ':', '-'],
+                [1.0, 1.0, 1.0],
+                [None] * 3,
+                [None] * 3,
+                f'b1={b1}, c={c}, d={d}',
+            ]
+            spectrum_row.append(spectrum_plot)
+            
+            if c == c_range[0] and d == d_range[0]:
+                min_idx = np.argmin(W_mag[afc_mask])
+                notch_freq = freq_g[afc_mask][min_idx]
+                notch_amp = W_mag[afc_mask][min_idx]
+                
+                freq_response_plot = [
+                    [freq_g[afc_mask], [notch_freq, notch_freq], [0, notch_freq]],
+                    [W_mag[afc_mask], [0, notch_amp], [notch_amp, notch_amp]],
+                    ['|W(iω)|', f"ω_0={notch_freq:.1f}", None],
+                    'ω', '|W(iω)|',
+                    ['m', 'b', 'b'],
+                    ['-', '--', '--'],
+                    [1.5, 0.9, 0.9],
+                    [None] * 3,
+                    [None] * 3,
+                    f'b1={b1} (ζ={b1/(2*np.sqrt(b2)):.2f})',
+                ]
+                freq_response_row.append(freq_response_plot)
+    
+    time_subplot_data.append([time_row])
+    spectrum_subplot_data.append([spectrum_row])
+    freq_response_subplot_data.append(freq_response_row)
+
+for tsd in time_subplot_data:
+    draw_plots(
+        rows=3,
+        cols=3,
+        width=12,
+        height=10,
+        subplot_data=tsd,
+        legend_loc='upper right',
+        legend_fontsize='xx-small'
+    )
+
+for ssd in spectrum_subplot_data:
+    draw_plots(
+        rows=3,
+        cols=3,
+        width=12,
+        height=10,
+        subplot_data=ssd,
+        legend_loc='upper right'
+    )
+
+draw_plots(
+    rows=1,
+    cols=3,
+    width=12,
+    height=3,
+    subplot_data=freq_response_subplot_data,
+    legend_loc='upper right'
+)
 
 # %%
 
