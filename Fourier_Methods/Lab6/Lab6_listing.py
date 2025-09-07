e411a76872bd0a76d2f3e23867049b2cbe942001
nsukhov <nsukhov22@gmail.com>
2025-09-08 01:49:13 +0300

    fm

--- Fourier_Methods/Lab6/Lab6_listing.py
+++ Fourier_Methods/Lab6/Lab6_listing.py
@@ -1,7 +1,7 @@
 # %%
 import numpy as np
 import cv2
-from scipy import fft
+from scipy.fft import fft2, fftshift, ifft2, ifftshift
 import matplotlib.pyplot as plt
 from scipy.signal import convolve2d
 
@@ -17,7 +17,7 @@ image_float = cv2.imread('1.png').astype(np.float64) / 255.0
 fft_channels = []
 for channel in range(3):
     channel_data = image_float[:, :, channel]
-    fft_channel = fft.fftshift(fft.fft2(channel_data))
+    fft_channel = fftshift(fft2(channel_data))
     fft_channels.append(fft_channel)
 fft_image = np.stack(fft_channels, axis=-1)
 
@@ -43,7 +43,7 @@ for channel in range(3):
     magnitude_channel = magnitude_filtered[:, :, channel]
     phase_channel = phase_spectrum[:, :, channel]
     complex_spectrum = magnitude_channel * np.exp(1j * phase_channel)
-    filtered_fft_channels.append(fft.ifft2(fft.ifftshift(complex_spectrum)))
+    filtered_fft_channels.append(ifft2(ifftshift(complex_spectrum)))
 
 reconstructed_image = np.stack(filtered_fft_channels, axis=-1)
 reconstructed_image = np.real(reconstructed_image)
@@ -93,182 +93,221 @@ negative_kernel = np.array([[0, 0, 0],
                             [0, 0, 0]])
 
 # %%
-fft_original = fft.fftshift(fft.fft2(img))
+fft_original = fftshift(fft2(img))
 log_magnitude_original = np.log1p(np.abs(fft_original))
+plt.imshow(log_magnitude_original, cmap='gray')
+plt.axis('off')
+plt.savefig(plt_folder + 'log.png', dpi=300, bbox_inches='tight')
+plt.show()
 
 # %%
-#туту надо сделать "Приведено изображение логарифма модуля образа исходной картинки."
+plt.rcParams['axes.titlesize'] = 12
+
+# %% [markdown]
+# Util func
 
 # %%
-plt.rcParams['axes.titlesize'] = 10
+def draw_pics3(kernel_f, name):
+    fig = plt.figure(figsize=(15, 18))
+
+    plt.subplot(3, 3, 1)
+    plt.axis('off')
+
+    plt.subplot(3, 3, 2)
+    plt.imshow(img, cmap='gray')
+    plt.title('Исходное изображение')
+    plt.axis('off')
+
+    plt.subplot(3, 3, 3)
+    plt.axis('off')
+
+    for i, N in enumerate(N_values):
+        kernel = kernel_f(N)
+        conv_result = convolve2d(img, kernel, mode='same', boundary='symm')
+        
+        k_h, k_w = kernel.shape
+        fft_size = (h + k_h - 1, w + k_w - 1)
+        fft_img_padded = fft2(img, s=fft_size)  
+        fft_kernel = fft2(kernel, s=fft_size)
+        
+        result_fft = ifft2(fft_img_padded * fft_kernel)
+        start_i = (fft_size[0] - h) // 2
+        start_j = (fft_size[1] - w) // 2
+        fft_result = np.real(result_fft[start_i:start_i+h, start_j:start_j+w])
+        
+        plt.subplot(3, 3, i+4)
+        plt.imshow(conv_result, cmap='gray')
+        plt.title(f'Обычная свертка: N={N}')
+        plt.axis('off')
+        
+        plt.subplot(3, 3, i+7)
+        plt.imshow(fft_result, cmap='gray')
+        plt.title(f'Фурье: N={N}')
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(plt_folder + name + '_comp.png', dpi=200, bbox_inches='tight')
+    plt.show()
+    
+    fig = plt.figure(figsize=(10, 18))
+
+    plt.subplot(4, 3, 1)
+    plt.axis('off')
+
+    plt.subplot(4, 3, 2)
+    plt.imshow(log_magnitude_original, cmap='gray')
+    plt.title('Логарифм модуля фурье-образа исходного изображения')
+    plt.axis('off')
+
+    plt.subplot(4, 3, 3)
+    plt.axis('off')
+
+    for i, N in enumerate(N_values):
+        kernel = kernel_f(N)
+        conv_result = convolve2d(img, kernel, mode='same', boundary='symm')
+        
+        k_h, k_w = kernel.shape
+        fft_size = (h + k_h - 1, w + k_w - 1)
+        fft_img_padded = fft2(img, s=fft_size)
+        fft_kernel = fft2(kernel, s=fft_size)
+        result_fft = ifft2(fft_img_padded * fft_kernel)
+        start_i = (fft_size[0] - h) // 2
+        start_j = (fft_size[1] - w) // 2
+        fft_result = np.real(result_fft[start_i:start_i+h, start_j:start_j+w])
+        
+        fft_kernel_shifted = fftshift(fft_kernel)
+        log_magnitude_kernel = np.log1p(np.abs(fft_kernel_shifted))
+        
+        fft_conv_spectrum = fftshift(fft2(conv_result))
+        log_magnitude_conv = np.log1p(np.abs(fft_conv_spectrum))
+        
+        fft_fft_spectrum = fftshift(fft2(fft_result))
+        log_magnitude_fft = np.log1p(np.abs(fft_fft_spectrum))
+        
+        plt.subplot(4, 3, i*3 + 4)
+        plt.imshow(log_magnitude_kernel, cmap='gray')
+        plt.title(f'Логарифм модуля\nфурье-образа ядра: N={N}')
+        plt.axis('off')
+        
+        plt.subplot(4, 3, i*3 + 5)
+        plt.imshow(log_magnitude_conv, cmap='gray')
+        plt.title(f'Лог. модуля фурье-обр. изобр.\nпосле обычной свертки: N={N}')
+        plt.axis('off')
+        
+        plt.subplot(4, 3, i*3 + 6)
+        plt.imshow(log_magnitude_fft, cmap='gray')
+        plt.title(f'Лог. модуля фурье-обр. изобр.\nпосле фурье: N={N}')
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(plt_folder + name + '_log.png', dpi=200, bbox_inches='tight')
+    plt.show()
 
 # %% [markdown]
 # #### Размытие по Гауссу
 
 # %%
-fig = plt.figure(figsize=(15, 18))
-
-plt.subplot(3, 3, 1)
-plt.axis('off')
-
-plt.subplot(3, 3, 2)
-plt.imshow(img, cmap='gray')
-plt.title('Исходное изображение')
-plt.axis('off')
-
-plt.subplot(3, 3, 3)
-plt.axis('off')
-
-for i, N in enumerate(N_values):
-    kernel = gaussian_kernel(N)
-    
-    conv_result = convolve2d(img, kernel, mode='same', boundary='symm')
-    conv_result = np.clip(conv_result, 0, 255)
-    
-    k_h, k_w = kernel.shape
-    fft_size = (h + k_h - 1, w + k_w - 1)
-    fft_img_padded = fft.fft2(img, s=fft_size)
-    fft_kernel = fft.fft2(kernel, s=fft_size)
-    result_fft = fft.ifft2(fft_img_padded * fft_kernel)
-    
-    start_i = (fft_size[0] - h) // 2
-    start_j = (fft_size[1] - w) // 2
-    fft_result = np.real(result_fft[start_i:start_i+h, start_j:start_j+w])
-    fft_result = np.clip(fft_result, 0, 255)
-    
-    plt.subplot(3, 3, i+4)
-    plt.imshow(conv_result, cmap='gray')
-    plt.title(f'Обычная свертка: N={N}')
-    plt.axis('off')
-    
-    plt.subplot(3, 3, i+7)
-    plt.imshow(fft_result, cmap='gray')
-    plt.title(f'Фурье: N={N}')
-    plt.axis('off')
-
-plt.tight_layout()
-plt.savefig(plt_folder + 'gaussian_comp.png', dpi=300, bbox_inches='tight')
-plt.show()
-
-# %%
-fig = plt.figure(figsize=(15, 18))
-
-plt.subplot(3, 1, 1)
-plt.imshow(log_magnitude_original, cmap='gray')
-plt.title('Логарифм модуля фурье-образа исходного изображения')
-plt.axis('off')
-
-for i, N in enumerate(N_values):
-    kernel = gaussian_kernel(N)
-    
-    conv_result = convolve2d(img, kernel, mode='same', boundary='symm')
-    conv_result = np.clip(conv_result, 0, 255)
-    
-    k_h, k_w = kernel.shape
-    fft_size = (h + k_h - 1, w + k_w - 1)
-    fft_img_padded = fft.fft2(img, s=fft_size)
-    fft_kernel = fft.fft2(kernel, s=fft_size)
-    result_fft = fft.ifft2(fft_img_padded * fft_kernel)
-    
-    start_i = (fft_size[0] - h) // 2
-    start_j = (fft_size[1] - w) // 2
-    fft_result = np.real(result_fft[start_i:start_i+h, start_j:start_j+w])
-    fft_result = np.clip(fft_result, 0, 255)
-    
-    fft_conv_spectrum = fft.fftshift(fft.fft2(conv_result))
-    log_magnitude_conv = np.log1p(np.abs(fft_conv_spectrum))
-    
-    fft_fft_spectrum = fft.fftshift(fft.fft2(fft_result))
-    log_magnitude_fft = np.log1p(np.abs(fft_fft_spectrum))
-    
-    plt.subplot(3, 3, i+4)
-    plt.imshow(log_magnitude_conv, cmap='gray')
-    plt.title(f'Обычная свертка: N={N}')
-    plt.axis('off')
-    
-    plt.subplot(3, 3, i+7)
-    plt.imshow(log_magnitude_fft, cmap='gray')
-    plt.title(f'Фурье: N={N}')
-    plt.axis('off')
-
-plt.tight_layout()
-plt.savefig(plt_folder + 'gaussian_log.png', dpi=300, bbox_inches='tight')
-plt.show()
+draw_pics3(gaussian_kernel, "gaussian")
 
 # %% [markdown]
 # #### Блочное размытие
 
 # %%
+draw_pics3(box_kernel, "gaussian")
 
+# %% [markdown]
+# Util func
+
+# %%
+def draw_pics(kernel, name):
+    conv_result = convolve2d(img, kernel, mode='same', boundary='symm')
+
+    k_h, k_w = kernel.shape
+    fft_size = (h + k_h - 1, w + k_w - 1)
+    fft_img_padded = fft2(img, s=fft_size)  
+    fft_kernel = fft2(kernel, s=fft_size)
+
+    result_fft = ifft2(fft_img_padded * fft_kernel)
+    start_i = (fft_size[0] - h) // 2
+    start_j = (fft_size[1] - w) // 2
+    fft_result = np.real(result_fft[start_i:start_i+h, start_j:start_j+w])
+
+    fig = plt.figure(figsize=(10, 8))
+
+    plt.subplot(1, 3, 1)
+    plt.imshow(img, cmap='gray')
+    plt.title('Исходное изображение')
+    plt.axis('off')
+
+    plt.subplot(1, 3, 2)
+    plt.imshow(conv_result, cmap='gray')
+    plt.title('Обычная свертка')
+    plt.axis('off')
+
+    plt.subplot(1, 3, 3)
+    plt.imshow(fft_result, cmap='gray')
+    plt.title('Фурье')
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(plt_folder + name + '_comp.png', dpi=200, bbox_inches='tight')
+    plt.show()
+
+    fft_kernel_shifted = fftshift(fft_kernel)
+    log_magnitude_kernel = np.log1p(np.abs(fft_kernel_shifted))
+
+    fft_conv_spectrum = fftshift(fft2(conv_result))
+    log_magnitude_conv = np.log1p(np.abs(fft_conv_spectrum))
+
+    fft_fft_spectrum = fftshift(fft2(fft_result))
+    log_magnitude_fft = np.log1p(np.abs(fft_fft_spectrum))
+
+    fig = plt.figure(figsize=(8, 10))
+
+    plt.subplot(2, 2, 1)
+    plt.imshow(log_magnitude_original, cmap='gray')
+    plt.title('Логарифм модуля фурье-образа\nисходного изображения')
+    plt.axis('off')
+
+    plt.subplot(2, 2, 2)
+    plt.imshow(log_magnitude_kernel, cmap='gray')
+    plt.title('Логарифм модуля фурье-образа ядра')
+    plt.axis('off')
+
+    plt.subplot(2, 2, 3)
+    plt.imshow(log_magnitude_conv, cmap='gray')
+    plt.title('Логарифм модуля фурье-образа\nпосле обычной свертки')
+    plt.axis('off')
+
+    plt.subplot(2, 2, 4)
+    plt.imshow(log_magnitude_fft, cmap='gray')
+    plt.title('Логарифм модуля фурье-образа\nпосле фурье')
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(plt_folder + name + '_log.png', dpi=200, bbox_inches='tight')
+    plt.show()
 
 # %% [markdown]
 # #### Увеличение резкости
 
 # %%
-kernel = sharp_kernel
-
-conv_result = convolve2d(img, kernel, mode='same', boundary='symm')
-conv_result = np.clip(conv_result, 0, 255)
-
-k_h, k_w = kernel.shape
-fft_size = (h + k_h - 1, w + k_w - 1)
-fft_img_padded = fft.fft2(img, s=fft_size)
-fft_kernel = fft.fft2(kernel, s=fft_size)
-result_fft = fft.ifft2(fft_img_padded * fft_kernel)
-
-start_i = (fft_size[0] - h) // 2
-start_j = (fft_size[1] - w) // 2
-fft_result = np.real(result_fft[start_i:start_i+h, start_j:start_j+w])
-fft_result = np.clip(fft_result, 0, 255)
-
-fft_kernel_shifted = fft.fftshift(fft_kernel)
-log_magnitude_kernel = np.log1p(np.abs(fft_kernel_shifted))
-
-fft_result_spectrum = fft.fftshift(fft.fft2(fft_result))
-log_magnitude_result = np.log1p(np.abs(fft_result_spectrum))
-
-fig, axes = plt.subplots(2, 3, figsize=(15, 10))
-
-axes[0, 0].imshow(img, cmap='gray')
-axes[0, 0].set_title('Исходное изображение')
-axes[0, 0].axis('off')
-
-axes[0, 1].imshow(conv_result, cmap='gray')
-axes[0, 1].set_title('Обычная свертка: Увеличение резкости')
-axes[0, 1].axis('off')
-
-axes[0, 2].imshow(fft_result, cmap='gray')
-axes[0, 2].set_title('Фурье: Увеличение резкости')
-axes[0, 2].axis('off')
-
-axes[1, 0].imshow(log_magnitude_original, cmap='gray')
-axes[1, 0].set_title('Логарифм модуля фурье-образа исходного изображения')
-axes[1, 0].axis('off')
-
-axes[1, 1].imshow(log_magnitude_kernel, cmap='gray')
-axes[1, 1].set_title('Логарифм модуля фурье-образа ядра')
-axes[1, 1].axis('off')
-
-axes[1, 2].imshow(log_magnitude_result, cmap='gray')
-axes[1, 2].set_title('Логарифм модуля фурье-образа результата')
-axes[1, 2].axis('off')
-
-plt.tight_layout()
-plt.savefig(plt_folder + 'sharp_comp.png', dpi=300, bbox_inches='tight')
-plt.show()
-
+draw_pics(sharp_kernel, "sharp")
 
 # %% [markdown]
 # #### Выделение краёв
 
 # %%
-
+draw_pics(edge_kernel, "edge")
 
 # %% [markdown]
 # #### Негатив
 
 # %%
+draw_pics(negative_kernel, "neg")
 
+# %% [markdown]
+# #### Сравнение гауссовского и блочного размытия
 
 # %%
 
